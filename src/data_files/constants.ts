7d6977ed0e724bcc15fba41c7912575a771db97b
MRK <64604054+mrk991000@users.noreply.github.com>
2024-12-23 12:39:01 +0330

    Update constants.ts

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,12 @@
 import ogImageSrc from "@images/social.png";
 
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "DABKA",
+  tagline: "Technology",
+  description: "DABKA offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+  description_short: "DABKA offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  url: "https://dabka.netlify.app/",
+  author: "DABKA",
 };
 
 export const SEO = {
@@ -34,6 +34,6 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description: "Equip your projects with DABKA's top-quality hardware tools and expert construction services. Trusted by industry leaders, DABKA offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
   image: ogImageSrc,
 };
